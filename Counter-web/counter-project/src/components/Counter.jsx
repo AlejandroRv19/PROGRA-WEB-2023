25dff1c7eb0af4a5f9866343c73bbd17177ae57c
AlejandroRv19 <98325607+AlejandroRv19@users.noreply.github.com>
2023-08-18 23:05:27 -0500

    Counter structure with React
    I deleted the folder Node_modules, because i have a problem with the console but you can Install again all the resoruces to see the project

---
+++ Counter-web/counter-project/src/components/Counter.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+
+export function Counter(){
+
+    
+    const CounterStyle ={
+        fontSize: "40px"
+        
+        
+     }
+
+  
+
+
+
+
+return(
+   
+
+    <>
+    
+     
+    <h2 style={CounterStyle}>0</h2>
+  
+    
+    
+    
+    
+    
+    </>
+
+
+
+
+)
+
+    
+}
---
+++ Counter-web/counter-project/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+
+export function Header(){
+
+
+    const HeaderStyle ={
+       color: "red",
+       fontSize: "60px"
+       
+       
+    }
+
+return(
+   
+
+    <>
+    
+     
+    <h1 style={HeaderStyle}>Counter</h1>
+    
+    
+    
+    
+    
+    
+    </>
+
+
+
+
+)
+
+    
+}
